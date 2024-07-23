d800fb3bd4f707a6e33119db43d192895641dad6
Jorge victor Portugal <161754678+JorgeVicPortugal@users.noreply.github.com>
2024-07-23 18:57:49 -0300

    primeiro

---
+++ resolucao-code/concat_dados.py
@@ -0,0 +1,6 @@
+primeiro_dado = input("Digite o primeiro dado: ")
+segundo_dado = input("Digite o segundo dado: ")
+
+resultado = primeiro_dado + segundo_dado
+
+print("A concatenação dos dados é:", resultado)
